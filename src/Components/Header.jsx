b6522851928a7f70557ecf1ccaafbfd48fcb800b
sameerram9321@gmail.com <sameerram9321@gmail.com>
2024-06-11 17:16:57 +0530

    Modifying the Buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,9 +18,12 @@ const Header = () => {
             </ul>
 
             <div className='flex gap-4 font-body1'>
-            <button className='border-[3px] border-black rounded-2xl px-4 py-2 hover:bg-slate-700 hover:text-white hover:font-medium hover:ring-4 ring-yellow-400'>Log in</button>
-            
-            <button className='border-[3px] border-black rounded-2xl px-4 py-2 hover:bg-slate-700 hover:text-white hover:font-medium hover:ring-4 ring-yellow-400'>Start now</button>
+            {/* <button className='border-[3px] border-black rounded-2xl px-4 py-2 hover:bg-slate-700 hover:text-white hover:font-medium hover:ring-4 ring-yellow-400'>Log in</button> */}
+      
+            {/* <button className='border-[3px] border-black rounded-2xl px-4 py-2 hover:bg-slate-700 hover:text-white hover:font-medium hover:ring-4 ring-yellow-400'>Start now</button> */}
+
+            <button class="btn-10"><span>Log In</span></button>
+            <button class="btn-10"><span>Sign Up</span></button>
             </div>
             
         </nav>
@@ -29,7 +32,8 @@ const Header = () => {
                 <p className='font-semibold mb-7'>Apron for business owners</p>
                 <p className='text-6xl font-bold font-body2 w-3/4'>FLIP PAYMENTS FROM A BARRIER TO A BOOSTER.</p>
                 <p className='mt-2 mb-9'>Sort,pay and reconcile your invoices in seconds.</p>
-                <button className='border-[3px] bg-black text-white border-black rounded-3xl px-24 py-2 hover:bg-slate-700 hover:font-medium hover:ring-4 ring-yellow-400'>START NOW</button>
+                <button class="btn-79"><span>START NOW</span></button>
+                
                 <hr className='border-dotted border-t-[3px]  border-black '/>
             </div>
             <div>
